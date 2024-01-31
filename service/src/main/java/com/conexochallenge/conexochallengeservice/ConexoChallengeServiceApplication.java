02e35e0492ec270dffbb80f3abed6fa1a1d5c84a
gfmota <gabrielfmota134@gmail.com>
2024-01-31 15:52:01 -0300

    feat: add service
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

---
+++ service/src/main/java/com/conexochallenge/conexochallengeservice/ConexoChallengeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.conexochallenge.conexochallengeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexoChallengeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexoChallengeServiceApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/conexochallenge/conexochallengeservice/ConexoChallengeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conexochallenge.conexochallengeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexoChallengeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
