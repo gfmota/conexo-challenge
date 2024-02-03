3f56207fe9ff749742a5577625241e8894aa62f3
gfmota <gabrielfmota134@gmail.com>
2024-02-03 00:04:01 -0300

    fix: remove broken test
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- app/src/pages/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '.';
-
-describe('index', () => {
-  it('should render correctly', () => {
-    render(<Home />);
-    expect(screen.getByText('Get started by editing')).toBeInTheDocument();
-  });
-});
